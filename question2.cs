121f094ebf057ff77804c5bc746589f94d1a1945
akhilamp123 <120724090+akhilamp123@users.noreply.github.com>
2023-01-18 21:25:22 +0530

    Add files via upload

---
+++ question2.cs
@@ -0,0 +1,30 @@
+﻿// accept a flight fare and booking time //
+
+Console.WriteLine("enter flight fares");
+double fare = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("enter the booking time");
+double time = Convert.ToDouble(Console.ReadLine());
+if (time >= 6 && time <= 9)
+{
+    fare = fare + (fare * 0.1);
+    Console.WriteLine(fare);
+
+}
+if (time >= 9 && time <= 17)
+{
+    fare = fare + (fare * 0.2);
+    Console.WriteLine(fare);
+}
+if (time >= 17 && time <= 23)
+{
+    fare = fare + (fare * 0.07);
+    Console.WriteLine(fare);
+}
+if (time >= 23 && time <= 6)
+{
+    fare = fare + (fare * 0.05);
+    Console.WriteLine(fare);
+}
+
+
