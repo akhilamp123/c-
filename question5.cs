43338a149324552823752d8ef3d42901fce797b5
akhilamp123 <120724090+akhilamp123@users.noreply.github.com>
2023-01-19 10:20:28 +0530

    Add files via upload

---
+++ question5.cs
@@ -0,0 +1,32 @@
+﻿// 5. accept the flight fares in decimal format and put it an array at even or odd positions.
+
+using System.Security.Cryptography;
+
+Console.WriteLine("enter the number of flight fares");
+int fare = Convert.ToInt32(Console.ReadLine());
+decimal[] arr = new decimal[fare*2];
+
+int even = 0;
+int odd = 1;
+Console.WriteLine("enter the elements");
+for (int i = 0; i < fare; i++)
+{
+    decimal value = Convert.ToDecimal(Console.ReadLine());
+    if (value % 2 == 0)
+    {
+        arr[even] = value;
+        even += 2;
+    }
+    else
+    {
+        arr[odd] = value;
+        odd += 2;
+    }
+}
+
+foreach (var a in arr)
+{
+    Console.WriteLine(a);
+}
+
+
