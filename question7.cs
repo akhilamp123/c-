cd261d514f4f076b4483d990ad6c909ee07706b9
akhilamp123 <120724090+akhilamp123@users.noreply.github.com>
2023-01-19 10:47:15 +0530

    Add files via upload

---
+++ question7.cs
@@ -0,0 +1,25 @@
+﻿// 7. accept passenger name and total miles travelled
+
+Console.WriteLine(" enter the name of the passenger:");
+string name = Console.ReadLine();
+
+Console.WriteLine("enter the total miles travelled:");
+int totalmiles = Convert.ToInt32(Console.ReadLine());
+
+if(totalmiles > 10000 && totalmiles < 20000)
+{
+    Console.WriteLine("The passenger is awarded 10 flyer points");
+}
+else if (totalmiles >20000 && totalmiles < 50000)
+{
+    Console.WriteLine("The passenger is awarded 20 flyer points");
+}
+else if (totalmiles > 50000 && totalmiles < 100000)
+{
+    Console.WriteLine("The passenger is awarded 30 flyer points");
+}
+else if(totalmiles > 100000)
+{
+    Console.WriteLine("The passenger is awarded 50 flyer points");
+}
+
